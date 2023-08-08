0a4174623420772911772335aa58a9072e7814dc
NehaBajjuri <105218064+NehaBajjuri@users.noreply.github.com>
2023-08-08 19:36:41 +0530

    js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     const registrationForm = document.getElementById('registrationForm');
     const userTableBody = document.querySelector('#userTable tbody');
 
+    // Load existing entries from localStorage
+    const savedEntries = JSON.parse(localStorage.getItem('userEntries')) || [];
+    savedEntries.forEach(entry => addUserToTable(entry));
+
     registrationForm.addEventListener('submit', function (event) {
         event.preventDefault();
-        
+
         const name = document.getElementById('name').value;
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
@@ -22,21 +25,34 @@ document.addEventListener('DOMContentLoaded', function () {
         const age = currentDate.getFullYear() - dobDate.getFullYear();
 
         if (age < 18 || age > 55) {
-            alert('Date of birth must be between ages 18 and 55!!');
+            alert('Date of birth must be between ages 18 and 55.');
             return;
         }
 
-        const newRow = document.createElement('tr');
-        newRow.innerHTML = `
-            <td>${name}</td>
-            <td>${email}</td>
-            <td>${password}</td>
-            <td>${dob}</td>
-            <td>${acceptTerms ? 'Yes' : 'No'}</td>
-        `;
-        userTableBody.appendChild(newRow);
+        const newUser = { name, email, password, dob, acceptTerms };
+        addUserToTable(newUser);
+        saveUserEntry(newUser);
 
         registrationForm.reset();
     });
-});
 
+    // Function to add user entry to the table
+    function addUserToTable(user) {
+        const newRow = document.createElement('tr');
+        newRow.innerHTML = `
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>${user.password}</td>
+            <td>${user.dob}</td>
+            <td>${user.acceptTerms ? 'Yes' : 'No'}</td>
+        `;
+        userTableBody.appendChild(newRow);
+    }
+
+    // Function to save user entry to localStorage
+    function saveUserEntry(user) {
+        const savedEntries = JSON.parse(localStorage.getItem('userEntries')) || [];
+        savedEntries.push(user);
+        localStorage.setItem('userEntries', JSON.stringify(savedEntries));
+    }
+});
